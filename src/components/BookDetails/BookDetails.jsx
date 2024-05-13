b3b2296df5479fbf6d0750e38dfc751c683ca712
jhraju <jhraju890@gmail.com>
2024-05-14 01:39:20 +0600

    added dynamic link card

--- src/components/BookDetails/BookDetails.jsx
+++ src/components/BookDetails/BookDetails.jsx
@@ -16,12 +16,37 @@ const [singleBook, setSingleBook] = useState(null)
     console.log(singleBook);
  const{ bookId, bookName, author, image , review, totalPages, rating, category, tags, publisher, yearOfPublishing} = singleBook || {};
     return (
-        <div>
-         <h1>single book details{singleBook?.bookName}</h1>
-          <div>
-           
+       
+        <div className="flex">
+         
+          <div className="bg-white opacity-80 w-[50%] ">
+           <img id="single-image" src={singleBook?.image} alt="" />
           </div>
-          <div></div>
+
+        <div>
+        <div className="w-[50%]">
+          <h1 className="text-[30px] font-bold">single book details{singleBook?.bookName}</h1>
+          <p className="font-bold text-xl">By:{singleBook?.author}</p>
+          <hr />
+          <p className="font-semibold text-xl">{singleBook?.category}</p>
+          <hr />
+          <p>Review:{singleBook?.review} </p>
+          <p className="font-bold">Tag: {singleBook?.tags}</p>
+          <hr />
+          <p>Number of pages: {singleBook?.totalPages}</p>
+          <p>Publisher: {singleBook?.publisher}</p>
+          <p>Year of Publishing: {singleBook?.yearOfPublishing}</p>
+          <p>Rating: {singleBook?.rating}</p>
+          
+        <button id="button" className="btn">Read</button>
+          <button id="button-2" className="btn ">Wishlist</button>
+    
+          </div>
+        </div>
+          
+        
+         
+         
         </div>
     );
 };
